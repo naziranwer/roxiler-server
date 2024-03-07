39ebf267a1a9206ddb19055944c25844915d9cb6
Nazir <anwernazir674@gmail.com>
2024-03-08 01:56:57 +0530

    cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const Products = require("./models/productsModel");
 const productsRoutes = require("./routes/productsRoutes");
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
